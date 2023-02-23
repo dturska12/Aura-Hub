962b83f6b074d176e1d9d4e6219b3c280b236772
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-02-23 13:58:36 -0600

    Update ListingWrapper.tsx

--- components/ListingWrapper/ListingWrapper.tsx
+++ components/ListingWrapper/ListingWrapper.tsx
@@ -2,46 +2,40 @@ import { useContract, useNFT } from "@thirdweb-dev/react";
 import { DirectListingV3, EnglishAuction } from "@thirdweb-dev/sdk";
 import Link from "next/link";
 import React from "react";
-import {
-    NFT_COLLECTION_ABI,
-    NFT_COLLECTION_ADDRESS,
-} from "../../const/contractAddresses";
+import { NFT_COLLECTION_ADDRESS } from "../../const/contractAddresses";
 import styles from "../../styles/Buy.module.css";
 import NFT from "../NFT/NFT";
 import Skeleton from "../Skeleton/Skeleton";
 
 type Props = {
-    listing: DirectListingV3 | EnglishAuction;
+  listing: DirectListingV3 | EnglishAuction;
 };
 
 /**
  * Accepts a listing and renders the associated NFT for it
  */
 export default function ListingWrapper({ listing }: Props) {
-    const { contract: nftContract } = useContract(
-        NFT_COLLECTION_ADDRESS,
-        NFT_COLLECTION_ABI
-    );
+  const { contract: nftContract } = useContract(NFT_COLLECTION_ADDRESS);
 
-    const { data: nft, isLoading } = useNFT(nftContract, listing.asset.id);
+  const { data: nft, isLoading } = useNFT(nftContract, listing.asset.id);
 
-    if (isLoading) {
-        return (
-            <div className={styles.nftContainer}>
-                <Skeleton width={"100%"} height="312px" />
-            </div>
-        );
-    }
-
-    if (!nft) return null;
-
+  if (isLoading) {
     return (
-        <Link
-            href={`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`}
-            key={nft.metadata.id}
-            className={styles.nftContainer}
-        >
-            <NFT nft={nft} />
-        </Link>
+      <div className={styles.nftContainer}>
+        <Skeleton width={"100%"} height="312px" />
+      </div>
     );
+  }
+
+  if (!nft) return null;
+
+  return (
+    <Link
+      href={`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`}
+      key={nft.metadata.id}
+      className={styles.nftContainer}
+    >
+      <NFT nft={nft} />
+    </Link>
+  );
 }
