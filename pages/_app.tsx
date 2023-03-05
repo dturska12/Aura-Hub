81609c14d324325270ca062d229a7f4559e3d926
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-03-04 19:50:18 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,12 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Navbar } from "../components/Navbar/Navbar";
 import NextNProgress from "nextjs-progressbar";
-import { NETWORK } from "../const/contractAddresses";
+import { NETWORK_ID } from "../const/contractAddresses";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={NETWORK}>
+    <ThirdwebProvider activeChain={NETWORK_ID}>
       {/* Progress bar when navigating between pages */}
       <NextNProgress
         color="var(--color-tertiary)"
